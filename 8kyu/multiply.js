b27956e716ab1f1597357738f7606be789614b09
Shane Ashley <shaneashleymf@gmail.com>
2022-08-18 20:28:09 +0100

    new codewars challenge

---
+++ 8kyu/multiply.js
@@ -0,0 +1,14 @@
+//Challenge
+//This code does not execute properly. Try to figure out why.
+// function multiply(a, b){
+//     return a * b
+//   }
+
+  //My solution
+
+  function multiply(a, b){
+    return  a * b
+    }
+    
+    multiply(5,6);
+    
